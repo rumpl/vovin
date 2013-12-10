52b13db3936d52c3666c566a0992d59ae5bb7f73
Djordje Lukic <lukic.djordje@gmail.com>
2013-12-10 01:06:26 +0100

    Indent using spaces

--- src/parser/therion/index.js
+++ src/parser/therion/index.js
@@ -2,6 +2,6 @@ var parser = require('./data');
 var fs = require('fs');
 
 fs.readFile('test.th', function (err, data) {
-	if (err) throw err;
-	console.log(JSON.stringify(parser.parse(data.toString()), null, 2));
+    if (err) throw err;
+    console.log(JSON.stringify(parser.parse(data.toString()), null, 2));
 });
